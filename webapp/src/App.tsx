0b2a14662966ae7dea45bc3026e488fca8da0e6a
safaiiry <arina.safronova.viantec@gmail.com>
2025-04-07 18:18:14 +0300

    Add prettier

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,10 @@
 export const App = () => {
   const ideas = [
-    { nick: "cool-idea-nick-1", name: "Idea 1", description: "Idea 1 Description..."},
-    { nick: "cool-idea-nick-2", name: "Idea 2", description: "Idea 2 Description..."},
-    { nick: "cool-idea-nick-3", name: "Idea 3", description: "Idea 3 Description..."},
-    { nick: "cool-idea-nick-4", name: "Idea 4", description: "Idea 4 Description..."},
-    { nick: "cool-idea-nick-5", name: "Idea 5", description: "Idea 5 Description..."},
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Idea 1 Description...' },
+    { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Idea 2 Description...' },
+    { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Idea 3 Description...' },
+    { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Idea 4 Description...' },
+    { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Idea 5 Description...' },
   ]
 
   return (
@@ -12,11 +12,12 @@ export const App = () => {
       <h1>Technical Support</h1>
       {ideas.map((idea) => {
         return (
-        <div key={idea.nick}>
-          <h2>{idea.name}</h2>
-          <p>{idea.description}</p>
-        </div> );
+          <div key={idea.nick}>
+            <h2>{idea.name}</h2>
+            <p>{idea.description}</p>
+          </div>
+        )
       })}
     </div>
-)
-};
+  )
+}
