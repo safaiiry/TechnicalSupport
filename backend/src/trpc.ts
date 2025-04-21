0a37631e0b2b8afab50ae11149337391fd8065c1
safaiiry <arina.safronova.viantec@gmail.com>
2025-04-21 20:16:04 +0300

    Add tickets generator to backend

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,67 +1,48 @@
 import { initTRPC } from '@trpc/server'
+import _ from 'lodash'
 
-const tickets = [
-  {
-    id: 1,
-    incNumber: 'INC0001',
-    typeName: 'Инцидент',
-    priority: 'Высокий',
-    description: 'Не работает интернет на 3 этаже',
-    stateName: 'Открыт',
-    contacts: 'Иванов И.И., +7 999 123-45-67',
-    dateCreated: '2025-04-18',
-    dateChanged: '2025-04-19',
-    performerFIO: 'Петров П.П.',
-  },
-  {
-    id: 2,
-    incNumber: 'INC0002',
-    typeName: 'Запрос',
-    priority: 'Средний',
-    description: 'Необходимо установить ПО',
-    stateName: 'В работе',
-    contacts: 'Сидорова А.А., +7 999 987-65-43',
-    dateCreated: '2025-04-17',
-    dateChanged: '2025-04-18',
-    performerFIO: 'Кузнецов В.В.',
-  },
-  {
-    id: 3,
-    incNumber: 'INC0003',
-    typeName: 'Инцидент',
-    priority: 'Низкий',
-    description: 'Мышь не работает',
-    stateName: 'Закрыт',
-    contacts: 'Тестов Т.Т., +7 912 345-67-89',
-    dateCreated: '2025-04-15',
-    dateChanged: '2025-04-16',
-    performerFIO: 'Лебедев И.И.',
-  },
-  {
-    id: 4,
-    incNumber: 'INC0004',
-    typeName: 'Запрос',
-    priority: 'Высокий',
-    description: 'Срочная замена принтера',
-    stateName: 'Ожидание',
-    contacts: 'Миронова Е.Ю., +7 916 456-78-90',
-    dateCreated: '2025-04-10',
-    dateChanged: '2025-04-12',
-    performerFIO: 'Фролов А.А.',
-  },
-  {
-    id: 5,
-    incNumber: 'INC0005',
-    typeName: 'Инцидент',
-    priority: 'Средний',
-    description: 'Сбой в работе 1С',
-    stateName: 'Открыт',
-    contacts: 'Дмитриев С.С., +7 926 321-54-76',
-    dateCreated: '2025-04-19',
-    dateChanged: '2025-04-20',
-    performerFIO: 'Кириллов М.М.',
-  },
+const typeNames = ['Инцидент', 'Запрос']
+const priorities = ['Высокий', 'Средний', 'Низкий']
+const descriptions = [
+  'Не работает интернет на 3 этаже',
+  'Необходимо установить ПО',
+  'Мышь не работает',
+  'Срочная замена принтера',
+  'Сбой в работе 1С',
 ]
+const stateNames = ['Открыт', 'В работе', 'Закрыт', 'Ожидание']
+const contacts = [
+  'Иванов И.И., +7 999 123-45-67',
+  'Сидорова А.А., +7 999 987-65-43',
+  'Тестов Т.Т., +7 912 345-67-89',
+  'Миронова Е.Ю., +7 916 456-78-90',
+  'Дмитриев С.С., +7 926 321-54-76',
+]
+const performers = ['Петров П.П.', 'Кузнецов В.В.', 'Лебедев И.И.', 'Фролов А.А.', 'Кириллов М.М.']
+
+function getRandomDate(base: string, offset = 0) {
+  const date = new Date(base)
+  date.setDate(date.getDate() + offset)
+  return date.toISOString().slice(0, 10)
+}
+
+const tickets = _.times(100, (i) => {
+  const createdOffset = _.random(-10, 0)
+  const changedOffset = createdOffset + _.random(0, 3)
+
+  return {
+    id: i + 1,
+    incNumber: `INC${(i + 1).toString().padStart(4, '0')}`,
+    typeName: _.sample(typeNames),
+    priority: _.sample(priorities),
+    description: _.sample(descriptions),
+    stateName: _.sample(stateNames),
+    contacts: _.sample(contacts),
+    dateCreated: getRandomDate('2025-04-20', createdOffset),
+    dateChanged: getRandomDate('2025-04-20', changedOffset),
+    performerFIO: _.sample(performers),
+  }
+})
 
 const trpc = initTRPC.create()
 
