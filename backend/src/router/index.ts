53bc6c2f6534759d9f027aa62ab5ba638f01efa4
safaiiry <arina.safronova.viantec@gmail.com>
2025-04-28 13:16:42 +0300

    Automatic File Index generation

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,10 +1,14 @@
 import { trpc } from '../lib/trpc'
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 import { getTicketTrpcRoute } from './getTicket'
 import { getTicketsTrpcRoute } from './getTickets'
+// @endindex
 
 export const trpcRouter = trpc.router({
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
   getTicket: getTicketTrpcRoute,
   getTickets: getTicketsTrpcRoute,
+  // @endindex
 })
 
 export type TrpcRouter = typeof trpcRouter
